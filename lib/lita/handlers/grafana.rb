322e5256697df1814fffaa3eaa8500f7910819b1
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-11-17 15:49:14 -0600

    wrote initial tests and added one passing statement

--- lib/lita/handlers/grafana.rb
+++ lib/lita/handlers/grafana.rb
@@ -1,7 +1,22 @@
 module Lita
   module Handlers
     class Grafana < Handler
-      # insert handler code here
+
+      config :url, required: true
+      #config :api_key, required: true
+      #config :s3_bucket, required: true
+      #config :s3_secret_access_key, required: true
+      #config :s3_prefix, required: true
+
+
+  route /^graf list$/i, :grafana, command: false, help: {
+    'graf list' => 'hello world'
+  }
+
+  def grafana(msg)
+    msg.reply("hello grafana") 
+  end
+  
 
       Lita.register_handler(self)
     end
--- spec/lita/handlers/grafana_spec.rb
+++ spec/lita/handlers/grafana_spec.rb
@@ -1,4 +1,9 @@
 require "spec_helper"
 
 describe Lita::Handlers::Grafana, lita_handler: true do
+  it { is_expected.to route_command('graf list').to(:grafana) }
+  it { is_expected.to route_command('graf db graphite-carbon-metrics').to(:grafana) }
+  it { is_expected.to route_command('graf db graphite-carbon-metrics now-12hr').to(:grafana) }
+  it { is_expected.to route_command('graf list production').to(:grafana) }
+  it { is_expected.to route_command('graf search elb').to(:grafana) }
 end
